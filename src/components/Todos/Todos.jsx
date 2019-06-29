01f8c1a630ec20a40bb09bc22c03d81815218e3d
peter <piotr.ludwig@typethink.com>
2019-06-29 17:33:53 +0200

    Todos component added to project

---
+++ src/components/Todos/Todos.jsx
@@ -0,0 +1,28 @@
+import React, { Component, createContext } from "react";
+
+const TodoContext = createContext();
+export const TodoConsumer = TodoContext.Consumer;
+
+
+class Todos extends Component {
+
+    state = {
+        input: "",
+        todos: [{ text: 'First task', timestamp: '12/02/2019', completed: false, id: '' },
+        { text: 'Second task', timestamp: '13/02/2019', completed: true, id: '' }]
+    };
+
+    render() {
+        const { children } = this.props;
+        return (
+            <div>
+                <TodoContext.Provider value={this.state}>
+                    {children}
+                </TodoContext.Provider>
+            </div>
+        )
+    }
+
+}
+
+export default Todos;
