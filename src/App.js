29a8636e8a0d335c80758774f98885846ee881d8
peter <piotr.ludwig@typethink.com>
2019-07-18 18:35:25 +0200

    Husky config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import Todos from './components/Todos/Todos'
+import Todos from './components/Todos/Todos';
 import './App.css';
 
 function App() {
   return (
     <div className="App container mt-5">
       <Todos>
+        sdfsd
         <Todos.Control />
         <Todos.TodoList>
           {({ todosFilter }) =>
-            todosFilter.map((todo) => (
+            todosFilter.map(todo => (
               <Todos.Item key={todo.id} id={todo.id}>
                 <Todos.Completed completed={todo.completed} />
                 <Todos.Text text={todo.text} />
                 <Todos.TimeStamp timeStamp={todo.timestamp} />
                 <Todos.Remove todoId={todo.id} />
               </Todos.Item>
-            ))}
+            ))
+          }
         </Todos.TodoList>
       </Todos>
     </div>
